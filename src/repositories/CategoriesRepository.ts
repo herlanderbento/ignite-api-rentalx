006439872457ec4f8ab8cb50ce1f27c2721e8658
herlanderbento <herlanderbento19@gmail.com>
2021-11-23 00:21:02 +0100

    Adicionando Principio de  substituicao de Liskov em categorias

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,19 +1,17 @@
 import { Category } from "../model/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../repositories/ICategoriesRepository";
 
-// Data transfer object
-interface ICreateCategoryDTO {
-  name: string;
-  description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
   constructor() {
     this.categories = [];
   }
 
-  create({ name, description }: ICreateCategoryDTO): void {
+  create({ description, name }: ICreateCategoryDTO): void {
     const category = new Category();
 
     Object.assign(category, {
