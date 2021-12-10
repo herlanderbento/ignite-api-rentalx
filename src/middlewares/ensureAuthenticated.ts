decd905da1830de141fea1609ac718b8ca35807f
herlanderbento <herlanderbento19@gmail.com>
2021-12-10 17:08:11 +0100

    Autenticacao de rotas

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
+
+interface IPayload {
+  sub: string;
+}
+async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new Error("Token missing");
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      "a6b4c721ed562bec74b20a02757791e0"
+    ) as IPayload;
+
+    const usersRepository = new UsersRepository();
+
+    const user = await usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User does not exists.");
+    }
+
+    next();
+  } catch (error) {
+    throw new Error("Invalid token!");
+  }
+}
+export { ensureAuthenticated };
