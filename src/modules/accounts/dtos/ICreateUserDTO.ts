71b3796d9ad507cc6714426e38b14bce0d829eda
herlanderbento <herlanderbento19@gmail.com>
2021-12-10 11:46:41 +0100

    criando repositorios de usuarios

---
+++ src/modules/accounts/dtos/ICreateUserDTO.ts
@@ -0,0 +1,12 @@
+// Data Transfer Oject (Objeto de Transferência de Dados):
+// é um padrão de projeto de software usado para transferir dados entre subsistemas de um software. DTOs são frequentemente usados em conjunção com objetos de acesso a dados para obter dados de um banco de dados.
+
+interface ICreateUserDTO {
+  name: string;
+  username: string;
+  email: string;
+  driver_license: string;
+  password: string;
+}
+
+export { ICreateUserDTO };
